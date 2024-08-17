89ae8d27e325705bb485c2d9af18603c016d49a0
chania <hannaferus@gmail.com>
2024-08-17 14:55:27 +0200

    Added LineCheckerSubsystem

---
+++ Source/BuiltToScale/Private/LineChecker.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LineChecker.h"
+
+void ULineChecker::SetTriggerPoints(TArray<ATriggerPoint*> TriggerPoints)
+{
+	for (ATriggerPoint* TriggerPoint : TriggerPoints)
+	{
+		if (TriggerPoint == nullptr)
+		{
+			continue;
+		}
+
+		if (!Lines.Contains(TriggerPoint->PositionY))
+		{
+			Lines.Add(TriggerPoint->PositionY);
+		}
+		Lines[TriggerPoint->PositionY].TriggerPoints.Add(TriggerPoint);
+	}
+}
+
+TArray<int32> ULineChecker::GetLinesToClean()
+{
+	TArray<int32> LinesToClean;
+	for (TTuple<int, FTriggerLine> TriggerPointLine : Lines)
+	{
+		bool bAddLine = true;
+		for (ATriggerPoint* TriggerPoint : TriggerPointLine.Value.TriggerPoints)
+		{
+			if (!TriggerPoint->bTaken)
+			{
+				bAddLine = false;
+				break;
+			}
+		}
+
+		if (bAddLine)
+		{
+			LinesToClean.Add(TriggerPointLine.Key);
+		}
+	}
+
+	return LinesToClean;
+}
---
+++ Source/BuiltToScale/Public/LineChecker.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BlueprintTools.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "LineChecker.generated.h"
+
+/**
+ * 
+ */
+USTRUCT()
+struct FTriggerLine
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TArray<ATriggerPoint*> TriggerPoints;
+};
+
+UCLASS()
+class BUILTTOSCALE_API ULineChecker : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void SetTriggerPoints(TArray<ATriggerPoint*> TriggerPoints);
+	
+	UFUNCTION(BlueprintCallable)
+	TArray<int32> GetLinesToClean();
+
+private:
+	TMap<int32, FTriggerLine> Lines;
+};
